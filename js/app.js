0b26632706527df7c3e338b11f4c6915e305d852
Jose Cuevas <josecueram@gmail.com>
2019-12-27 21:52:21 -0600

    Added basic hold button logic

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const scores = [0,0]
 let roundScore = 0
 let activePlayer = 0
 const $buttonRoll = document.querySelector('#button-roll')
+const $buttonHold = document.querySelector('#button-hold')
 const $dice = document.querySelector('#dice')
 const $playerPanels = document.querySelectorAll('.c-player-panel')
 
@@ -9,7 +10,6 @@ const $playerPanels = document.querySelectorAll('.c-player-panel')
 // ? Roll button logic
 $buttonRoll.addEventListener('click', () => {
     // 1. Get DOM score to interact with & generate random number
-    const $currentScore = document.querySelector(`#current-${activePlayer}`)
     let diceValue = Math.floor(Math.random() * 6) + 1
     console.log(diceValue)
 
@@ -20,15 +20,33 @@ $buttonRoll.addEventListener('click', () => {
     // 3. Update the round score IF the rolled number was not 1
     if (diceValue !== 1) {
         roundScore += diceValue
-        $currentScore.textContent = roundScore
+        document.querySelector(`#current-${activePlayer}`).textContent = roundScore
     } else {
-        roundScore = 0
-        $currentScore.textContent = roundScore
-        $dice.style.opacity = '0'
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
-        
-        $playerPanels.forEach(panel => {
-            panel.classList.toggle('c-player-panel--active')
-        })
+        nextPlayer()
     }
 })
+
+// ? Hold button logic
+$buttonHold.addEventListener('click', () => {
+    scores[activePlayer] += roundScore
+    const playerScore = document.querySelector(`#score-${activePlayer}`)
+    playerScore.textContent = scores[activePlayer]
+
+    if (scores[activePlayer] >= 10) {
+        document.querySelector(`#name-${activePlayer}`).textContent = 'WINNER!'
+    } else {
+        nextPlayer()
+    }
+})
+
+function nextPlayer () {
+    roundScore = 0
+    document.querySelector(`#current-${activePlayer}`).textContent = roundScore
+    $dice.style.opacity = '0'
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0    
+    
+    $playerPanels.forEach(panel => {
+         panel.classList.toggle('c-player-panel--active')
+    })
+}
+
